5271278a72c767214645ea8de7a16b6c74203f5f
Robin Geissmann <robingeissmann@hotmail.com>
2019-10-15 11:52:45 +0200

    switchable interface sw04

---
+++ src/main/java/ch/hslu/sw04/Switchable.java
@@ -0,0 +1,11 @@
+package ch.hslu.sw04;
+
+/*** Schnittstellen NIE ohne Dokumentation!!!* Hier nur aus Platzgründen entfallen.*/
+
+public interface Switchable
+{
+    void switchOn();
+    void switchOff();
+    boolean isSwitchedOn();
+    boolean isSwitchedOff();
+}
